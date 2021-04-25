82090818d817ff7d3525d9d9b67a112a07466eee
Ryan Fernandez <rfernan3@trinity.edu>
2021-04-25 13:30:35 -0500

    Bin View template

--- server/app/controllers/Application.scala
+++ server/app/controllers/Application.scala
@@ -5,11 +5,29 @@ import javax.inject._
 import shared.SharedMessages
 import play.api.mvc._
 
+//            Potential Types for Bins Method
+// case class User(username: String)
+// case class Bin(name: String, articles: Seq[String])
+
 @Singleton
 class Application @Inject()(cc: ControllerComponents) extends AbstractController(cc) {
 
   def index = Action { implicit request =>
     Ok(views.html.index())
   }
+
+  //            Rough Draft Method for Getting Bins
+  //
+  // def bins = Action { implicit request => 
+  //   request.body.asJson.map { body => 
+  //               Json.fromJson[User](body) match {
+  //                   case JsSuccess(user, path) => {   
+  //                       val bins = WRDRB.getBins(user.username)
+  //                       Ok(Json.toJson(bins)
+  //                   }
+  //                   case e @ JsError(_) => Ok(Json.toJson(false))
+  //               }
+  //           }.getOrElse(Redirect(routes.Application.index()))
+  // }
 
 }
--- server/public/javascript/react-main.js
+++ server/public/javascript/react-main.js
@@ -1,7 +1,8 @@
 import { ce } from './react-common.js';
 import MainComponent from './components/MainComponent.js';
+import BinComponent from './components/BinComponent.js';
 
 ReactDOM.render(
-  ce(MainComponent),
+  ce(BinComponent),
   document.getElementById('react-root'),
 );
