3ee9a02301d48f38d3fdb8ad94c2ebc0236f7d8a
Thomas Lauerman <tom@lauerman.dev>
2021-04-27 19:35:55 -0500

    Remove comments, mock loadBins

--- server/public/javascript/components/BinComponent.js
+++ server/public/javascript/components/BinComponent.js
@@ -19,33 +19,28 @@ export default class BinComponent extends React.Component {
          * @type {object}
          * @property {Bin[]} bins
          * @property {string} searchText The current Bin search text.
-         * @property {string} selected The currently selected Bin.
+         * @property {?string} selected The currently selected Bin.
          */
         this.state = {
+            bins: [],
+            searchText: "",
+            selected: null,
+        };
+    }
+
+    componentDidMount() {
+        this.loadBins(this.props.username);
+    }
+
+    loadBins(username){
+        this.setState({
             bins: [
                 { name: "Your Favorites", articles: ["Default Shirt"] },
                 { name: "Work", articles: ["White Shirt", " Black Pants", "Black Shoes"] }, 
                 { name: "Weekend", articles: ["Blue Hat", "White Shirt", "Black Tie", "Blue Pants"] },
                 { name: "Home", articles: ["Gray Shirt", "Black Pants"] }
-            ],
-            searchText: "",
-            selected: "Your Favorites",
-        };
-    }
-
-    // componentDidMount() {
-    //     this.loadBins(this.props.username);
-    // }
-
-    loadBins(username){
-        fetch(binRoute, {
-            method: "POST",
-            headers: {'Content-Type': 'application/json', 'Csrf-Token': csrfToken},
-            body: JSON.stringify({ username })
-        }).then(res => res.json()).then(data => {
-            this.setState({ bins: data })
-            this.loadBins(this.props.username)
-        })
+            ]
+        });
     }
     
     changeHandler(e) {
@@ -56,7 +51,6 @@ export default class BinComponent extends React.Component {
         return ce('div', null, 
         ce('h1', null, 'Bins'),
         ce('button', { onClick: e => this.setState({ selected: "Your Favorites" })}, 'Logout'),
-        // ce('p', null, `The CSRF Token is ${csrfToken}`),
         ce('img', { src: versionedAsset('images/favicon.png') }),
         ce('input', {type: 'text', id: 'searchText', value: this.state.searchText, onChange: e => this.changeHandler(e)}),
         ce('button', {onClick: e => console.log(this.state.searchText)}, 'Search'),
@@ -65,11 +59,16 @@ export default class BinComponent extends React.Component {
         ce('div', null, this.props.username + "'s Bins"),
         ce('br'),
         ce('div', null, this.state.bins.map((bin, index) => ce('div', {key: index, onClick: e => this.setState({ selected: bin.name })}, bin.name))),
-        ce('br'),
-        ce('div', null, 'Viewing: '),
-        ce('br'),
-        ce('div', null, this.state.bins.find((bin) => {return bin.name == this.state.selected}).name),
-        ce('ul', null, this.state.bins.find((bin) => {return bin.name == this.state.selected}).articles.map((article, index) => ce('li', {key: index}, article))),
+        (() => {
+            if (this.state.selected != null)
+                return ce('div', null,
+                    ce('br'),
+                    ce('div', null, 'Viewing: '),
+                    ce('br'),
+                    ce('div', null, this.state.bins.find((bin) => {return bin.name == this.state.selected}).name),
+                    ce('ul', null, this.state.bins.find((bin) => {return bin.name == this.state.selected}).articles.map((article, index) => ce('li', {key: index}, article))),
+                );
+        })(),
         );
     }
 }
