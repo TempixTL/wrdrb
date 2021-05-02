f559d97b759d8737e601838498abe90f7167bfc6
Thomas Lauerman <tom@lauerman.dev>
2021-05-01 19:03:17 -0500

    Show NavBar on MainComponent

--- server/public/javascript/components/MainComponent.js
+++ server/public/javascript/components/MainComponent.js
@@ -1,4 +1,5 @@
 import { ce, csrfToken, versionedAsset } from '../react-common.js';
+import NavBarComponent from './NavBarComponent.js';
 import AuthenticationComponent from './authentication/AuthenticationComponent.js';
 
 /**
@@ -7,7 +8,26 @@ import AuthenticationComponent from './authentication/AuthenticationComponent.js
  * Edit this component to change what is rendered on the index page.
  */
 export default class MainComponent extends React.Component {
+  navbarOutfitLogClicked() {
+    console.log('Outfit Log clicked');
+  }
+
+  navbarMyWardrobeClicked() {
+    console.log('My Wardrobe clicked');
+  }
+
+  navbarMyBinsClicked() {
+    console.log('My Bins clicked');
+  }
+
   render() {
-    return ce(AuthenticationComponent);
+    return ce('div', null,
+      ce(NavBarComponent, {
+        onOutfitLogClicked: () => this.navbarOutfitLogClicked(),
+        onMyWardrobeClicked: () => this.navbarMyWardrobeClicked(),
+        onMyBinsClicked: () => this.navbarMyBinsClicked(),
+      }),
+      ce(AuthenticationComponent),
+    );
   }
 }
