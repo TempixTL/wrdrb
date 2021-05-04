be88cac3a7eefb078d7f3f2a28a39627b7bc3cc0
Thomas Lauerman <tom@lauerman.dev>
2021-05-04 12:12:09 -0500

    Reset react-main.js to create MainComponent

--- server/public/javascript/react-main.js
+++ server/public/javascript/react-main.js
@@ -1,9 +1,7 @@
 import { ce } from './react-common.js';
 import MainComponent from './components/MainComponent.js';
-import BinComponent from './components/BinComponent.js';
-import LogComponent from './outfitLog.js'
 
 ReactDOM.render(
-  ce(LogComponent),
+  ce(MainComponent),
   document.getElementById('react-root'),
 );
