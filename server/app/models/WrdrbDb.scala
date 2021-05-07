502770178cceba7e270605b2c3242361ad2fcc80
tempixtl <tom@lauerman.dev>
2021-05-07 17:39:33 -0500

    Stub out WrdrbDb class for talking to the database

---
+++ server/app/models/WrdrbDb.scala
@@ -0,0 +1,16 @@
+package models
+
+import slick.jdbc.PostgresProfile.api._
+import scala.concurrent.{Future, ExecutionContext}
+
+/**
+  * The main class used by the Wrdrb app to talk to the backend database.
+  * 
+  * This class handles database queries and translation of database values
+  * into models used in this application.
+  *
+  * @param db The database which stores persistent data for the app.
+  * @param ec The execution context with which to run asynchronous code.
+  */
+class WrdrbDb(db: Database)(implicit ec: ExecutionContext) {}
+
