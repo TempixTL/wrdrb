686b1a9f86b65052503bef9e2e569d56da232d7f
tempixtl <tom@lauerman.dev>
2021-05-07 18:21:00 -0500

    Implement user login and registration in the DB

--- server/app/models/WrdrbDb.scala
+++ server/app/models/WrdrbDb.scala
@@ -3,6 +3,8 @@ package models
 import slick.jdbc.PostgresProfile.api._
 import scala.concurrent.{Future, ExecutionContext}
 
+import codegen.Tables._
+
 /**
   * The main class used by the Wrdrb app to talk to the backend database.
   * 
@@ -13,8 +15,43 @@ import scala.concurrent.{Future, ExecutionContext}
   * @param ec The execution context with which to run asynchronous code.
   */
 class WrdrbDb(db: Database)(implicit ec: ExecutionContext) {
-  def validateLogin(username: String, password: String) = ???
-  def validateRegister(username: String, password: String) = ???
+
+  /**
+    * Asynchronously validates that the given login credentials are correct.
+    *
+    * @param username The username of the authenticating user.
+    * @param password The password of the authenticating user in plaintext.
+    * @return `Some(User)` of the validated user on success, `None` for invalid
+    * credentials or other errors.
+    */
+  def validateLogin(username: String, password: String): Future[Option[User]] = {
+    val matches = db.run(Users.filter(row => row.username === username && row.password === password).result)
+    matches.map(_ match {
+      case user::Nil => Some(User(user.id, user.username))
+      case _ => None
+    })
+  }
+
+  /**
+    * Asynchronously validates that the given registration credentials are
+    * valid, and if so, adds that user to the database.
+    *
+    * @param username The username of the registering user.
+    * @param password The password of the registering user in plaintext.
+    * @return `Some(User)` of the newly registered user on success, `None` for
+    * invalid credentials or other errors.
+    */
+  def validateRegister(username: String, password: String): Future[Option[User]] = {
+    val matches = db.run(Users.filter(row => row.username === username).result)
+    matches.flatMap(_ match {
+      case Nil =>
+        val insertQuery = Users returning Users.map(_.id)
+        db.run(insertQuery += UsersRow(-1, username, password))
+          .map(userId => Some(User(userId, username)))
+      case _ => Future(None)
+    })
+  }
+
   // et ceterea
 }
 
