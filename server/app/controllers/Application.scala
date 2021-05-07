712a3cf96faf67e641d78613a8bc51f1bc5e3a5d
tempixtl <tom@lauerman.dev>
2021-05-07 18:43:28 -0500

    Add database setup for controller

--- server/app/controllers/Application.scala
+++ server/app/controllers/Application.scala
@@ -4,16 +4,26 @@ import javax.inject._
 
 import shared.SharedMessages
 import play.api.mvc._
+import scala.concurrent.{Future, ExecutionContext}
+import play.api.db.slick.DatabaseConfigProvider
+import play.api.db.slick.HasDatabaseConfigProvider
+import slick.jdbc.JdbcProfile
+import slick.jdbc.PostgresProfile.api._
+import models.WrdrbDb
 
 //            Potential Types for Bins Method
 // case class User(username: String)
 // case class Bin(name: String, articles: Seq[String])
 
 @Singleton
-class Application @Inject()(cc: ControllerComponents) extends AbstractController(cc) {
+class Application @Inject()(protected val dbConfigProvider: DatabaseConfigProvider, cc: ControllerComponents)
+    (implicit ec: ExecutionContext) extends AbstractController(cc) with HasDatabaseConfigProvider[JdbcProfile] {
+  private val database = new WrdrbDb(db)
 
-  def index = Action { implicit request =>
-    Ok(views.html.index())
+  def index = Action.async { implicit request =>
+    Future {
+      Ok(views.html.index())
+    }
   }
 
   //            Rough Draft Method for Getting Bins
