d0e985357e166676ccca593a663d0e61fc3c756f
Ryan Fernandez <rfernan3@trinity.edu>
2021-05-09 14:27:17 -0500

    getBins, addBin, deleteBin, addArticleToBin, removeArticleFromBin

--- server/app/models/WrdrbDb.scala
+++ server/app/models/WrdrbDb.scala
@@ -52,6 +52,81 @@ class WrdrbDb(db: Database)(implicit ec: ExecutionContext) {
     })
   }
 
-  // et ceterea
+  //                  ------ Bin Methods ------
+  //
+  /**
+    * Asynchronously returns all the user's bins.
+    *
+    * @param username The username of the requesting user.
+    * @return `Seq[Bin]` on success, `Nil` on failure.
+    */
+  def getAllBins(username: String): Future[Seq[Bin]] = {
+    db.run(
+      (for {
+        user <- Users if user.username === username
+        bin <- Bins if bin.user_id === user.id
+      } yield {
+        bin
+      }).result
+    ).map(bins => bins.map(bin => Bin(bin.id, bin.user_id, bin.name)))
+  }
+
+  /**
+    * Asynchronously add a bin.
+    *
+    * @param userId The id of the requesting user.
+    * @param name The name of the new bin.
+    * @return `Some(Bin)` on success, `None` on already exists.
+    */
+  def addBin(userId: Int, name: String): Future[Option[Bin]] = {
+    val matches = db.run(Bins.filter(row => row.user_id === userId && row.name === name).result)
+    matches.flatMap(_ match {
+      case Nil =>
+        val insertQuery = Bins returning Bins.map(_.id)
+        db.run(insertQuery += BinsRow(-1, userId, name))
+          .map(binId => Some(Bin(binId, userId, name)))
+      case _ => Future(None)
+    })
+  }
+
+  /**
+    * Asynchronously delete a bin.
+    *
+    * @param binId The id of the bin to delete.
+    * @return `true` on success, `false` on failure.
+    */
+  def deleteBin(binId: Int): Future[Boolean] = {
+    db.run(Bins.filter(_.id === binId).delete).map(count => count > 0)
+  }
+
+  /**
+    * Asynchronously add an existing article to an existing bin.
+    *
+    * @param binId The id of the bin.
+    * @param articleId The id of the article to add.
+    * @return `true` on success, `false` on failure.
+    */
+  def addArticleToBin(binId: Int, articleId: Int): Future[Boolean] = {
+    val matches = db.run(ArticlesBins.filter(row => row.id === binId && row.article_id === articleId).result)
+    matches.flatMap(_ match {
+      case Nil => 
+        val insertQuery = ArticlesBins returning ArticlesBins.map(_.id)
+        db.run(insertQuery += ArticlesBinsRow(-1, binId, articleId))
+        true
+      case _ => false
+    })
+  }
+
+  /**
+    * Asynchronously remove an existing article from an existing bin.
+    *
+    * @param binId The id of the bin.
+    * @param articleId The id of the article to remove.
+    * @return `true` on success, `false` on failure.
+    */
+  def removeArticleFromBin(binId: Int, articleId: Int): Future[Boolean] = {
+    db.run(ArticleBins.filter(row => row.id === binId && row.articleId === articleId).delete).map(count => count > 0)
+  }
+
 }
 
