c70e827a94640cdf8bcee506b1be367fb847393f
Thomas Lauerman <tom@lauerman.dev>
2021-05-10 17:33:29 -0500

    Add pretty styles to login

--- server/public/javascript/components/authentication/AuthenticationComponent.js
+++ server/public/javascript/components/authentication/AuthenticationComponent.js
@@ -1,4 +1,4 @@
-import { ce } from '../../react-common.js';
+import { ce, versionedAsset } from '../../react-common.js';
 import UserFormComponent from './UserFormComponent.js';
 import '../../models/UserFormComponentSubmitCallback.js';
 
@@ -25,27 +25,42 @@ export default class AuthenticationComponent extends React.Component {
   }
 
   render() {
-    return ce('div', { className: 'row' },
-      ce('div', { className: 'col s12 m8 offset-m2'},
-        ce('div', { className: 'card' },
-          ce('div', { className: 'card-tabs' },
-            ce('ul', { className: 'tabs tabs-fixed-width' },
-              ce('li', { className: 'tab' }, ce('a', { href: '#register-form', className: 'active' }, 'Register')),
-              ce('li', { className: 'tab' }, ce('a', { href: '#login-form' }, 'Login')),
-            ),
+    return ce('div', null,
+      ce('div', { className: 'row'},
+        ce('div', { className: 'col s12' },
+          ce('div', { className: 'valign-wrapper' },
+            ce('img', { src: versionedAsset('images/logo.svg') }),
+            ce('span', { className: 'grey-text' }, 'Organizing your wardrobe.'),
           ),
-          ce('div', { className: 'card-content' },
-            ce('div', { id: 'register-form' },
-              ce(UserFormComponent, {
-                title: 'Register',
-                submitLabel: 'Register',
-                onSubmit: this.props.onRegisterFormSubmit }),
+        ),
+      ),
+      ce('div', { className: 'row' },
+        ce('div', { className: 'col s12 m6 offset-m3 center-align' },
+          ce('img', { src: versionedAsset('images/fashion-tshirts-colour.svg'), className: 'responsive-img' }),
+        ),
+      ),
+      ce('div', { className: 'row' },
+        ce('div', { className: 'col s12 m8 offset-m2'},
+          ce('div', { className: 'card' },
+            ce('div', { className: 'card-tabs' },
+              ce('ul', { className: 'tabs tabs-fixed-width' },
+                ce('li', { className: 'tab' }, ce('a', { href: '#register-form', className: 'active' }, 'Register')),
+                ce('li', { className: 'tab' }, ce('a', { href: '#login-form' }, 'Login')),
+              ),
             ),
-            ce('div', { id: 'login-form' },
-              ce(UserFormComponent, {
-                title: 'Login',
-                submitLabel: 'Login',
-                onSubmit: this.props.onLoginFormSubmit }),
+            ce('div', { className: 'card-content' },
+              ce('div', { id: 'register-form' },
+                ce(UserFormComponent, {
+                  title: 'Register',
+                  submitLabel: 'Register',
+                  onSubmit: this.props.onRegisterFormSubmit }),
+              ),
+              ce('div', { id: 'login-form' },
+                ce(UserFormComponent, {
+                  title: 'Login',
+                  submitLabel: 'Login',
+                  onSubmit: this.props.onLoginFormSubmit }),
+              ),
             ),
           ),
         ),
