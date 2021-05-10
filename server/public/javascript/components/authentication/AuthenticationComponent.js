4088ee260f8b984975cf96ce83e96af1bf18f3c8
tempixtl <tom@lauerman.dev>
2021-05-10 13:31:47 -0500

    Add styles to Auth page

--- server/public/javascript/components/authentication/AuthenticationComponent.js
+++ server/public/javascript/components/authentication/AuthenticationComponent.js
@@ -20,16 +20,36 @@ export default class AuthenticationComponent extends React.Component {
     this.props;
   }
 
+  componentDidMount() {
+    M.AutoInit();
+  }
+
   render() {
-    return ce('div', null,
-      ce(UserFormComponent, {
-        title: 'Login',
-        submitLabel: 'Login',
-        onSubmit: this.props.onLoginFormSubmit }),
-      ce(UserFormComponent, {
-        title: 'Register',
-        submitLabel: 'Register',
-        onSubmit: this.props.onRegisterFormSubmit }),
+    return ce('div', { className: 'row' },
+      ce('div', { className: 'col s12 m8 offset-m2'},
+        ce('div', { className: 'card' },
+          ce('div', { className: 'card-tabs' },
+            ce('ul', { className: 'tabs tabs-fixed-width' },
+              ce('li', { className: 'tab' }, ce('a', { href: '#register-form', className: 'active' }, 'Register')),
+              ce('li', { className: 'tab' }, ce('a', { href: '#login-form' }, 'Login')),
+            ),
+          ),
+          ce('div', { className: 'card-content' },
+            ce('div', { id: 'register-form' },
+              ce(UserFormComponent, {
+                title: 'Register',
+                submitLabel: 'Register',
+                onSubmit: this.props.onRegisterFormSubmit }),
+            ),
+            ce('div', { id: 'login-form' },
+              ce(UserFormComponent, {
+                title: 'Login',
+                submitLabel: 'Login',
+                onSubmit: this.props.onLoginFormSubmit }),
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
