e139929f58d2469ae55356cd579c3a44ac5b6c4b
Thomas Lauerman <tom@lauerman.dev>
2021-05-18 16:30:50 -0500

    Output compiled JS to client/dist

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -16,6 +16,6 @@ module.exports = {
   },
   output: {
     filename: 'react-main.js',
-    path: path.resolve(__dirname, '..', 'server', 'public', 'javascript'),
+    path: path.resolve(__dirname, 'dist'),
   },
 };
